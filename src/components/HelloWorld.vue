1267a08f593c96d90a7baf26e026ce246d17ef76
davon6 <damignot@gmail.com>
2025-02-25 20:37:51 +0100

    hot_fix main

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 
-const count = ref(0)
+//const count = ref(0)
 
 // Function to trigger animations on scroll
 const handleScroll = () => {
