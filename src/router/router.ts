f2b77da22921534fda151edce85790c189d46a02
davon6 <damignot@gmail.com>
2025-07-26 17:49:24 +0200

    hot_fix_nav

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,7 +5,7 @@ import Professionnels from '../components/Professionnels.vue'
 import type { RouterScrollBehavior } from 'vue-router';
 
 // define scrollBehavior properly
-const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
+const scrollBehavior: RouterScrollBehavior = (to, savedPosition) => {
   if (to.hash) {
     return {
       el: to.hash,
