6e3a8460dd9e99f58168d6b7549153795334db03
davon6 <damignot@gmail.com>
2025-08-13 21:18:34 +0200

    pro layout mobile

--- src/components/Professionnels.vue
+++ src/components/Professionnels.vue
@@ -528,7 +528,7 @@ main.container {
 
   /* Service cards */
   .section-card {
-    max-width: 75% !important; 
+    max-width: 95% !important; 
     padding: 0.8em !important;
     gap: 0.5em !important;
     box-shadow: none !important;
